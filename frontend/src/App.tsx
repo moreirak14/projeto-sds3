edb4af2d412264eab16151252aac5d5a5d60299d
moreirak14 <kaiquemoreiracloud@gmail.com>
2021-09-08 23:03:40 -0300

    Charts errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import BarChart from "components/BarChart";
+/* import BarChart from "components/BarChart"; */
 import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
+/* import DonutChart from "components/DonutChart"; */
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
